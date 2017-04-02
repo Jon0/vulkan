034b55a66c0d8a8cd032132109b7d3a57bca7588
Jon0 <Jono4728@gmail.com>
2017-04-02 23:18:38 +1200

    pipeline

---
+++ src/pipeline.cpp
@@ -0,0 +1,5 @@
+#include "pipeline.h"
+
+Pipeline::Pipeline() {
+    
+}
---
+++ src/pipeline.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+
+class Pipeline {
+public:
+    Pipeline();
+
+
+private:
+    VkInstance instance;
+    
+};
